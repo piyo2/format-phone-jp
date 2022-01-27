311f3cdf8c19c0c6a2395a66a582a5146ebcc6cd
Shinichiro Yabu <scin.ichi@gmail.com>
2022-01-27 13:50:29 +0900

    PHP8に対応

--- build/build.php
+++ build/build.php
@@ -4,9 +4,9 @@ require_once __DIR__ . '/../vendor/autoload.php';
 // ルールファイル生成方法
 //
 //   1. 市外局番の一覧 Word 形式を開く
-//   2. Word ファイルの「市外局番」列を Excel にコピー
+//   2. Word ファイルの「市外局番」列（ヘッダ除く）を Excel にコピー
 //   3. Excel で「CSV UTF-8」形式で保存
-// 
+//
 // 市外局番の一覧
 // http://www.soumu.go.jp/main_sosiki/joho_tsusin/top/tel_number/shigai_list.html
 $src = __DIR__ . '/source.csv';
@@ -31,19 +31,7 @@ $fixedRules = [
 	'(90)(....)(.+)',
 ];
 
-$tsv = file_get_contents($src);
-$cols = ['code'];
-
-$parser = new sharapeco\CSVParser\CSVParser([
-	'csv_encoding' => 'UTF-8',
-]);
-$rows = $parser->parse($tsv, $cols);
-
-$rows = array_filter($rows, function($row) {
-	return preg_match('/\\A[0-9]+\\z/', trim($row['code']));
-});
-
-$codes = array_map(function($row) { return trim($row['code']); }, $rows);
+$codes = file($src, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 $codes = array_values(array_unique($codes));
 
 // 市外局番が長い順に並べる
